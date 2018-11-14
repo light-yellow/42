ea5714d4eb3cf57a518ae1b308aca4dd078a7516
nazym <nazymanteater@gmail.com>
2018-11-14 16:39:04 +0300

    added files for ex00

---
+++ d04/ex00/ft_iterative_factorial.c
@@ -0,0 +1,14 @@
+int	ft_iterative_factorial(int nb)
+{
+	int result;
+
+	result = 1;
+	if (nb < 0 || nb > 12)
+		return (0);
+	while (nb > 1)
+	{
+		result *= nb;
+		nb -= 1;
+	}
+	return (result);
+}
