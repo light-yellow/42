b7a002aaf95cc78175f3c17a84f976fa95987b26
nazym <nazymanteater@gmail.com>
2018-11-15 16:25:32 +0300

    added files for ex03

---
+++ d04/ex03/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int	ft_recursive_power(int nb, int power);
+
+int	main(void)
+{
+	for (int num = -3; num < 4; num += 1)
+	{
+		for (int pow = -2; pow < 4; pow += 1)
+		{
+			printf("%d to the power of %d is %d\n", num, pow, ft_recursive_power(num, pow));
+		}
+	}
+}
