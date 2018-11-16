892b5f084bf476becdf7da6563f233d8cadfaa84
nazym <nazymanteater@gmail.com>
2018-11-17 01:08:36 +0300

    added files for ex08

---
+++ d04/ex08/ft_eight_queens_puzzle.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+
+int	is_available(int *board, int collumn, int row)
+{
+	int i;
+
+	i = 1;
+	while (i <= collumn)
+	{
+		if (board[collumn - i] == row)
+			return (0);
+		else if (board[collumn - i] == row - i) 
+			return(0);
+		else if (board[collumn - i] == row + i)
+			return (0);
+		i += 1;
+	}
+	return (1);
+}
+
+int	ft_count_possibilities(int *board, int filled)
+{
+	int row;
+	int placements;
+
+	row = 1;
+	placements = 0;
+	if (filled == 7)
+		return (1);
+	while (row <= 8)
+	{
+		if (is_available(board, filled + 1, row))
+		{
+			board[filled + 1] = row;
+			placements += ft_count_possibilities(board, filled + 1);
+		}
+		row += 1;
+	}
+	return (placements);
+}
+
+int	ft_eight_queens_puzzle(void)
+{
+	int i;
+	int board[8];
+	int placements;
+
+	i = 1;
+	placements = 0;
+	while (i <= 8)
+	{
+		board[0] = i;
+		placements += ft_count_possibilities(board, 0);
+		i += 1;
+	}
+	return (placements);
+}
---
+++ d04/ex08/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int	ft_eight_queens_puzzle(void);
+
+int	main(void)
+{
+	printf("There are %d possible placements of eight queens.", ft_eight_queens_puzzle());
+	return (0);
+}
