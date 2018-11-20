2ee4977fd6bb1b7fea5b78e37099d9915ed3b8d8
nazym <nazymanteater@gmail.com>
2018-11-21 01:39:28 +0300

    added files for ex03

---
+++ d08/ex03/main.c
@@ -0,0 +1,10 @@
+#include "ft_abs.h"
+#include <stdio.h>
+
+int main(void)
+{
+    printf("absolute value of 42 is %d\n", ABS(42));
+    printf("absolute value of -42 is %d\n", ABS(-42));
+    printf("absolute value of 0 is %d\n", ABS(0));
+    return (0);
+}
